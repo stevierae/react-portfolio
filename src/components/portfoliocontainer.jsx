bd228213ed5fbe7e29c96aa4e3124c070af6555f
Stevie <stefaniefisher88@gmail.com>
2023-08-17 15:24:35 -0400

    added layout builder

---
+++ src/components/portfoliocontainer.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Header from './header';
+import About from './pages/about';
+import Portfolio from './pages/portfolio';
+import Contact from './pages/contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      <main className="mx-3">{renderPage()}</main>
+    </div>
+  );
+}
